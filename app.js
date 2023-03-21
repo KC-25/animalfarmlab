c7d83e66dd3156b49ed0cc6fda5e9d221c7e2124
KC-25 <kierrconnell@g.crookcountyschools.org>
2023-03-21 14:25:20 -0700

    Finished today's todo's

---
+++ app.js
@@ -0,0 +1,49 @@
+'use strict';
+
+// TODO: assign `catButton` a value by selecting the button labeled `Make Cat` from the DOM
+
+const catButton = document.getElementById('make-cat');
+catButton.addEventListener('click', function(){
+  // TODO: assign `newCat a value by creating a new list item DOM element
+  const newCat = document.createElement('li');
+  // TODO: assign the list item's text to be 'Cat' or the name of a cat you know.
+  newCat.textContent = 'Sphinx';
+  const cats = document.getElementById('cats');
+  cats.appendChild(newCat);
+});
+
+const dogButton = document.getElementById('make-dog');
+dogButton.addEventListener('click', function (){
+  const newDog = document.createElement('li');
+  newDog.textContent = 'Black Lab';
+  // TODO: assign `dogs` a value by selecting the second unordered list by its id
+ const dogs = document.getElementById('dogs');
+//  const dogs;
+
+  // TODO: append the newly created list item to the dogs list
+  dogs.appendChild(newDog);
+});
+
+const sheepButton = document.getElementById('make-sheep');
+// TODO: add an event listener to the sheepButton that calls `handleMakingSheep` when clicked
+
+sheepButton.addEventListener("click", handleMakingSheep);
+
+function handleMakingSheep(){
+  // TODO: make a new list item, assign its text content to `Sheep` or the name of a sheep you know, and append it to the third unordered list.
+const newSheep = document.createElement('li');
+newSheep.textContent = 'Lamb';
+const sheeps = document.getElementById('sheeps');
+sheeps.appendChild(newSheep)
+};
+
+// TODO: create an event listener for the `Make Horse` Button that calls a function named `handleMakingHorse`
+
+const horseButton = document.getElementById('make-horse');
+horseButton.addEventListener("click", handleMakinghorse);
+function handleMakinghorse(){
+    const newhorse = document.createElement('li');
+    newhorse.textContent = 'Stallion';
+    const horses = document.getElementById('horses');
+    horses.appendChild(newhorse)
+    };
